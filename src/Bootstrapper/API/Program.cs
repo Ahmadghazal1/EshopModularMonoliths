394e0ccc1f28b3382a1b796fe886c6913991db34
Ahmadghazal1 <74017377+Ahmadghazal1@users.noreply.github.com>
2025-07-16 23:54:55 +0300

    Develop Shared Domain For Tactical DDD Abstraction(Entity,ValueObject,Aggregate)

---
+++ src/Bootstrapper/API/Program.cs
@@ -0,0 +1,28 @@
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            //Add services to the container.
+
+            builder.Services
+                .AddCatalogModule(builder.Configuration)
+                .AddBasketModule(builder.Configuration)
+                .AddOrderingModule(builder.Configuration);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.
+                 UseCatalogModule()
+                .UseBasketModule()
+                .UseOrderingModule();
+
+            app.Run();
+        }
+    }
+}
